577714142af7663332082c68a740ed240f2216dd
gustaaa <gustayo24@gmail.com>
2022-10-18 12:17:21 +0700

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/person.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    Provider(
+      create: (_) => Person(name: "gusta", age: 20),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyNamePage(),
+    );
+  }
+}
+
+class MyNamePage extends StatelessWidget {
+  const MyNamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Provider"),
+      ),
+      body: Center(
+        child: Text(
+          '''
+          Hi ${Provider.of<Person>(context).name}!
+          You are ${Provider.of<Person>(context).age} years old''',
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/person.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Person with ChangeNotifier {
+  //attribute
+  String name;
+  int age;
+  //constructor
+  Person({
+    required this.name,
+    required this.age,
+  });
+
+  void increaseAge() {
+    this.age++;
+    notifyListeners();
+  }
+}
